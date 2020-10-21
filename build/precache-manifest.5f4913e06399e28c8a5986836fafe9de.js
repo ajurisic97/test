bfce6ae1a7d2800925f366ec729da368bb71e276
Andelo Jurisic <ajurisic@pmfst.hr>
2020-10-21 16:05:35 +0200

    Dodaj sve

---
+++ build/precache-manifest.5f4913e06399e28c8a5986836fafe9de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "317ea394a69723383fa0bcd35d99cde7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c5777aea26bbcbe4d905",
+    "url": "/static/css/main.90ffd6ca.chunk.css"
+  },
+  {
+    "revision": "75e5ba4b459a4b80655a",
+    "url": "/static/js/2.7d5328f6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7d5328f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5777aea26bbcbe4d905",
+    "url": "/static/js/main.fcdf4c7d.chunk.js"
+  },
+  {
+    "revision": "82664534ad3b166a4ff3",
+    "url": "/static/js/runtime-main.5763738a.js"
+  }
+]);
